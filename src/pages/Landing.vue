77f9cbfee3345b2448a215f4cd8a95b974d3ef59
Neal Matta <nealmatt@umich.edu>
2019-11-20 17:05:10 -0500

    Ready to create the actual website

---
+++ src/pages/Landing.vue
@@ -0,0 +1,26 @@
+<template>
+<div class="container">
+  <h1 class="title is-size-1">Volleyball Rotations</h1>
+</div>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+		}
+	},
+	methods: {
+	},
+	components: {
+	},
+	mounted(){
+	}
+};
+</script>
+
+<style>
+	.title {
+		text-decoration: underline;
+	}
+</style>
